34fb6c0490230f15de58f21ef615eeff9ab2c335
mrmatil <niedzielnygracz1234@gmail.com>
2019-07-13 22:26:43 +0200

    Initial Commit

---
+++ Aplikacja Mobilna WZR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aplikacja Mobilna WZR
+//
+//  Created by Mateusz Łukasiński on 13/07/2019.
+//  Copyright © 2019 Mateusz Łukasiński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
