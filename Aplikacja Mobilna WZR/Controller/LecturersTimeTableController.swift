fdec9f721c91a4cadfcfb874fe54f92ccc106695
mrmatil <niedzielnygracz1234@gmail.com>
2019-07-28 22:43:09 +0200

    added Lecturers View Controller

---
+++ Aplikacja Mobilna WZR/Controller/LecturersTimeTableController.swift
@@ -0,0 +1,36 @@
+//
+//  LecturersTimeTableController.swift
+//  Aplikacja Mobilna WZR
+//
+//  Created by Mateusz Łukasiński on 28/07/2019.
+//  Copyright © 2019 Mateusz Łukasiński. All rights reserved.
+//
+
+import UIKit
+
+class LecturersTimeTableController: UIViewController {
+
+    
+    
+    //UIActions:
+    @IBAction func weekPickerChanged(_ sender: UISegmentedControl) {
+    }
+    @IBAction func dayPickerChanged(_ sender: UISegmentedControl) {
+    }
+    
+    //IBOutlets:
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var lecturersTableView: UITableView!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
