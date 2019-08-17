7bcb23262fbdb2bc3d584e722718bdf15373350d
mrmatil <niedzielnygracz1234@gmail.com>
2019-08-17 17:25:53 +0200

    added some comments to RemoveDuplicates class, added deselect row to noticeBoard view controller and fixed height constraint for custom notice board cell

--- Aplikacja Mobilna WZR/Controller/FullTimeControllers/NoticeBoardController.swift
+++ Aplikacja Mobilna WZR/Controller/FullTimeControllers/NoticeBoardController.swift
@@ -89,5 +89,9 @@ extension NoticeBoardController:UITableViewDataSource,UITableViewDelegate{
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     
 }
--- Aplikacja Mobilna WZR/Model/TimeTables/RemoveDuplicates.swift
+++ Aplikacja Mobilna WZR/Model/TimeTables/RemoveDuplicates.swift
@@ -11,25 +11,29 @@ import Foundation
 class RemoveDuplicates{
     
     static func removeFT(data:[ClassesArray])->[ClassesArray]{
+        
         //variables:
         let weeks:[Int] = [1,2]
         let days:[String] = ["poniedziałek","wtorek","środa","czwartek","piątek"]
         
         var dataWithoutDuplicates = [ClassesArray]()
         for week in weeks{
+            
             for day in days{
                 //getting array of one day
-                var tempArray = [ClassesArray]() //array of lessons during one day without duplicates
+                var tempArray = [ClassesArray]() //array of classes during one day without duplicates
                 for x in data{
                     if x.nameOfTheDay==day && x.typeOfWeek==week{
                         tempArray.append(x)
                     }
                 }
                 
+                //if more than one class during the day it is searching for "duplicates" and merging it
                 if tempArray.count>1{
                     var temp:ClassesArray = tempArray[0]
                     for y in 0...tempArray.count-2{
                         if tempArray[y+1].className == temp.className && tempArray[y+1].classroom == temp.classroom && tempArray[y+1].lecturer == temp.lecturer{
+                            
                             temp = ClassesArray(group: tempArray[y+1].group,
                                                 className: tempArray[y+1].className,
                                                 lecturer: tempArray[y+1].lecturer,
@@ -38,19 +42,30 @@ class RemoveDuplicates{
                                                 classroom: tempArray[y+1].classroom,
                                                 typeOfWeek: week,
                                                 nameOfTheDay: day)
+                            
                         }
                         else{
+                            
                             dataWithoutDuplicates.append(temp)
                             temp = tempArray[y+1]
+                            
                         }
+                        
                     }
+                    
                     dataWithoutDuplicates.append(temp)
                     
                 }
+                    
+                // if only one class during the day it only append to array that one
                 else if tempArray.count == 1{
                     dataWithoutDuplicates.append(tempArray[0])
                 }
+                
+                //if there is no class during the day nothing happens
+                
             }
+            
         }
         
         
