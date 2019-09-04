32126b3685739cf35c96a838a4a7149e8834f410
mrmatil <niedzielnygracz1234@gmail.com>
2019-09-04 19:56:51 +0200

    added initial View Controller

---
+++ Aplikacja Mobilna WZR/Controller/InitialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InitialViewController.swift
+//  Aplikacja Mobilna WZR
+//
+//  Created by Mateusz Łukasiński on 04/09/2019.
+//  Copyright © 2019 Mateusz Łukasiński. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
